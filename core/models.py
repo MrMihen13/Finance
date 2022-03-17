f031f0ce17458646f8cad7623474f43bf16ed201
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-03-17 13:21:58 +0300

    New models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Name', blank=False, null=False)
+    limit = models.DecimalField(verbose_name='Limit', max_digits=12, decimal_places=2, blank=True, null=True)
+
+
