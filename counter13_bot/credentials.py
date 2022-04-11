2f0793fe39f1dd5abd690c6f47a5de9b4e603447
MrMihen13 <87896079+MrMihen13@users.noreply.github.com>
2022-04-11 20:23:27 +0300

    Delete counter13_bot directory

--- counter13_bot/credentials.py
+++
@@ -1,9 +0,0 @@
-# Do not remove these 2 lines:
-from django.conf import settings
-
-
-if settings.TELEGRAM['TOKEN']:
-    BOT_TOKEN = settings.TELEGRAM['TOKEN']
-    APP_NAME = settings.TELEGRAM['BOT_USERNAME']
-else:
-    raise Exception('Telegram bot token not set in environ')
