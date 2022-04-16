e0503f4a209983f9108feb5406e89a831c8ce8e9
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-04-16 22:36:45 +0300

    Api for telegram bot

--- api_for_tg/views.py
+++ api_for_tg/views.py
@@ -1,17 +1,37 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, response, status
 
 from core import serializers, custom_permissions, models
 
 
 class AddCostTelegramAPI(generics.CreateAPIView):
-    serializer_class = serializers.CostSerializer
+    serializer_class = serializers.TelegramCostSerializer
     permission_classes = (custom_permissions.HasTelegramToken, )
     queryset = models.Cost.objects.all()
 
+    def post(self, request, *args, **kwargs):
+        if request.data.get('telegram_user_id') is None:
+            return response.Response(data={'error': 'user_id is None'}, status=status.HTTP_400_BAD_REQUEST)
+        data = {
+            'telegram_user_id': request.data.get('telegram_user_id'),
+            'name': request.data.get('name'),
+            'amount': request.data.get('amount'),
+        }
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+        return response.Response(data={'msg': 'Successful'}, status=status.HTTP_200_OK)
 
-class CostListTelegramAPI(generics.GenericAPIView):
-    permission_classes = (permissions.AllowAny, )
-    serializer_class = serializers.CostListSerializer
 
-    def queryset(self, request, *args, **kwargs):
-        return models.Cost.objects.filter(user_id__telegram_id=request.data['user_id']).all()
+class CostListTelegramAPI(generics.ListAPIView):
+    permission_classes = (custom_permissions.HasTelegramToken, )
+    serializer_class = serializers.TelegramCostSerializer
+    queryset = models.Cost.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        if request.data.get('telegram_user_id') is None:
+            return response.Response(data={'error': 'user_id is None'}, status=status.HTTP_400_BAD_REQUEST)
+        data = self.queryset.filter(telegram_user_id=request.data.get('telegram_user_id'))
+        serializer = self.serializer_class(data=data, many=True)
+        serializer.is_valid()
+        return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+
