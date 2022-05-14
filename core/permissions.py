c615e428652e7e7f601c929021623d5468b1571a
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-05-14 21:33:44 +0300

    Permissions for the different user`s rate plan

--- core/permissions.py
+++ core/permissions.py
@@ -1,3 +1,5 @@
+# TODO Написать краткое описание для каждого класса и функции
+
 from rest_framework import permissions
 
 
@@ -5,3 +7,22 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.pk == request.user
 
+
+class OnlyProfessionalRatePlan(permissions.BasePermission):
+    message = {
+        'errors': 'Rate plan error',  # TODO Скорректировать сообщение ограничении функционала тарифного плана
+        'message': 'Ваш тарифный план не включает в себя данную функцию. '
+    }
+
+    def has_permission(self, request, view):
+        return request.user.rate_plan == 'professional'
+
+
+class BaseOrProfessionalRatePlan(permissions.BasePermission):
+    message = {
+        'errors': 'Rate plan error',  # TODO Скорректировать сообщение ограничении функционала тарифного плана
+        'message': 'Ваш тарифный план не включает в себя данную функцию.'
+    }
+
+    def has_permission(self, request, view):
+        return request.user.rate_plan != 'free'
