78fbc9e09a8ab970165bd07eb615231cde8c17c6
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-05-14 21:33:01 +0300

    Custom paginator with next and previous month link

--- core/pagination.py
+++ core/pagination.py
@@ -1,3 +1,5 @@
+# TODO Написать краткое описание для каждого класса и функции
+
 from rest_framework import pagination, response, status
 from rest_framework.exceptions import NotFound
 
@@ -7,12 +9,24 @@ class CustomPagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-    def get_paginated_response(self, data):
-        return response.Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link(),
-            },
-            'count': self.page.paginator.count,
-            'results': data
-        }, status=status.HTTP_200_OK)
+    def get_paginated_response(self, data, next_month=None, prev_month=None):
+        if next_month is not None and prev_month is not None:
+            return response.Response({
+                'links': {
+                    'next_page': self.get_next_link(),
+                    'prev_page': self.get_previous_link(),
+                    'next_month': next_month,
+                    'prev_month': prev_month,
+                },
+                'count': self.page.paginator.count,
+                'results': data
+            }, status=status.HTTP_200_OK)
+        else:
+            return response.Response({
+                'links': {
+                    'next_page': self.get_next_link(),
+                    'prev_page': self.get_previous_link(),
+                },
+                'count': self.page.paginator.count,
+                'results': data
+            }, status=status.HTTP_200_OK)
