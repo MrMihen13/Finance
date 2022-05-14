161bb94089f87dae9d455a089d94ed1d79913a4b
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-05-14 21:31:45 +0300

    New fild for the CostAdmin and custom queryset for the CategoryAdmin

--- core/admin.py
+++ core/admin.py
@@ -5,13 +5,19 @@ from core import models
 
 @admin.register(models.Cost)
 class CostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category_id', 'user_id')
+    list_display = ('id', 'name', 'category_id', 'user_id', 'amount')
     list_filter = ('category_id', 'user_id')
 
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'limit', 'user_id')
+
+    def get_queryset(self, request):
+        queryset = super(CategoryAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        return queryset.filter(user_id=request.user.id)
 
 
 @admin.register(models.CustomUser)
