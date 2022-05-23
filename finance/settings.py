f183bbdacc0d9c0f2e0c4e283ab5b5bf9d22e4bc
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-05-23 21:56:39 +0300

    Changes for pre-alfa version

--- finance/settings.py
+++ finance/settings.py
@@ -126,10 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Simplified static file serving.
-
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-
 # Logging
 
 LOGGING = {
