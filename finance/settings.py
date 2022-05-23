3717562d532782d88b35e719ca374c93fb0a5aa3
Konatnstin Merezhnikov <kmerezhnikov@emphasoft.com>
2022-05-23 22:01:37 +0300

    Changes for pre-alfa version

--- finance/settings.py
+++ finance/settings.py
@@ -121,6 +121,7 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 
