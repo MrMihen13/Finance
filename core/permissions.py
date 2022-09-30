07dcc32ae63b8153081fbc4c19210fe6ad0965d7
MrMihen13 <87896079+MrMihen13@users.noreply.github.com>
2022-09-30 12:24:40 +0300

    Update permissions.py

--- core/permissions.py
+++ core/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.user_id == request.user
+        return obj.pk == request.user
 
 
 class OnlyProfessionalRatePlan(permissions.BasePermission):
